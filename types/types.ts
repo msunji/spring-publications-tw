4e4b67790dce89b95c70e16fd999c92716e1190c
Marge <marge.consunji@gmail.com>
2023-05-29 18:05:27 +0800

    Add custom types

---
+++ types/types.ts
@@ -0,0 +1,35 @@
+export type Thumbnail = {
+  id: string;
+  width: number;
+  height: number;
+  url: string;
+  filename: string;
+  size: number;
+  type: string;
+  thumbnails: {
+    small: {
+      url: string,
+      width: number,
+      height: number
+    },
+    large: {
+      url: string,
+      width: number,
+      height: number
+    },
+    full: {
+      url: string,
+      width: number,
+      height: number
+    }
+  }
+}
+
+export type Book = {
+  id: string;
+  title: string;
+  author: string;
+  thumbnail: Array<Thumbnail>;
+  price: number;
+  desc: string;
+}
