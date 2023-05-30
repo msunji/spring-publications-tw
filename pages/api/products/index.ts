d4567d49c04255f1bcc7be039ee539a774efc6a0
Marge <marge.consunji@gmail.com>
2023-05-30 09:53:11 +0800

    Put in try catch block

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -4,8 +4,13 @@ import { getBooks } from '@/lib/airtable';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Array<Book>>
+  res: NextApiResponse<Array<Book> | { error: string }>
 ) {
-  const books = await getBooks();
-  res.status(200).json(books);
+  try {
+    const books = await getBooks();
+    res.status(200).json(books);
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Unknown error.";
+    res.json({ error: message });
+  }
 }
