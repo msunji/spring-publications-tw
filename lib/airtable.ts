ade5f9d45c43a77283a962c7f66c6f8d529904c7
Marge <marge.consunji@gmail.com>
2023-05-30 09:35:12 +0800

    Add helper functions for getting Airtable data

---
+++ lib/airtable.ts
@@ -0,0 +1,68 @@
+const Airtable = require("airtable");
+import { Thumbnail, Book } from '@/types/types';
+
+function initAirtable() {
+  const base = new Airtable({ apiKey:process.env.AIRTABLE_TOKEN }).base(process.env.AIRTABLE_BASE_ID);
+  return base;
+}
+
+function getFieldData(record:any = {}) {
+  const id:string = record.getId();
+  const title:string = record.get("Title");
+  const author:string = record.get("Author");
+  const thumbnail:Array<Thumbnail> = record.get("Thumbnail");
+  const price:number = record.get("Price");
+  const desc:string = record.get("Description");
+
+  const book:Book = {
+    id,
+    title,
+    author,
+    thumbnail,
+    price,
+    desc
+  };
+
+  return book;
+}
+
+export function getBooks() {
+  const base = initAirtable();
+  const inventory:Array<any> = [];
+  return new Promise<Array<any>>((resolve, reject) => {
+    base("Product List")
+      .select({
+        fields: ["Title", "Author", "Thumbnail", "Price", "Description", "Publish"],
+        filterByFormula: "{Publish} = 1",
+      })
+      .eachPage(
+        function page(records:Array<any>, fetchNextPage: () =>{}) {
+          records.forEach((record:any = {}) => {
+            const book = getFieldData(record);
+            inventory.push(book);
+          });
+          fetchNextPage();
+        }, function done(err:string) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          return resolve(inventory);
+      });
+  });
+}
+
+export function getBookId(id:string) {
+  const base = initAirtable();
+  return new Promise<Book>((resolve, reject) => {
+    base("Product List")
+      .find(id, function(err:string, record: any = {}) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const book = getFieldData(record);
+        return resolve(book);
+      })
+  })
+}
