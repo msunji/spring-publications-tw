ef02eaecbefc3a0eb828e8d37a6efb6d27619339
Marge <marge.consunji@gmail.com>
2023-05-30 12:12:34 +0800

    Change 404 text to Chinese

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Custom404() {
-  return <h1>Page cannot be found</h1>;
+  return (
+    <section className="text-center">
+      <h1>404 錯誤</h1>
+      <p>很抱歉，找不到網頁。請<Link href="/">回到首頁</Link>。</p>
+    </section>
+  )
 }
