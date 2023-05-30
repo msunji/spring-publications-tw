4ab471ae467ab2da1d14d4b94880f67f28722137
Marge <marge.consunji@gmail.com>
2023-05-30 15:20:22 +0800

    Fix image unconfigured host error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "v5.airtableusercontent.com",
+        port: "",
+        pathname: "/v1/17/17/**/**"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
