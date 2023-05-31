3fc0cf35ff308c5923a55e78745158079916b617
Marge <marge.consunji@gmail.com>
2023-05-31 16:45:23 +0800

    Edit copy

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,8 @@ export default function Custom404() {
   return (
     <section className="text-center">
       <h1>404 錯誤</h1>
-      <p>很抱歉，找不到網頁。請<Link href="/">回到首頁</Link>。</p>
+      <p>很抱歉，找不到網頁</p>
+      <p>請<Link href="/">回到首頁</Link>。</p>
     </section>
   )
 }
