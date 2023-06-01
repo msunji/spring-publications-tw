61fe8f782bc6315f7fabb6a18802dcc05b97ad9d
Marge <marge.consunji@gmail.com>
2023-06-01 15:05:26 +0800

    Zhuzh up h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home({ data } : { data: Array<Book>}) {
   return (
     <section>
       <div>
-        <h1 className="mb-10 text-center text-3xl">全部商品</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8">
+        <h1 className="flex flex-row before:border-b before:flex-1 before:m-auto before:mr-6 before:border-neutral-300 before:max-w-[40%] after:flex-1 after:border-b after:m-auto after:ml-6 after:max-w-[40%] after:border-neutral-300 mb-10 text-center text-3xl text-sec">全部商品</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-8">
           { data.map((book:Book) => <Thumbnail key={book.id} {...book} />
           )}
         </div>
