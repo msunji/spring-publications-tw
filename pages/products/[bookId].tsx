bfb8751c5fa4069c4e19ffd38b587781da5d4ad5
Marge <marge.consunji@gmail.com>
2023-06-01 16:39:37 +0800

    Add buttons and style

--- pages/products/[bookId].tsx
+++ pages/products/[bookId].tsx
@@ -32,35 +32,35 @@ export default function Page({ data } : { data:Book }) {
               style={{objectFit: "cover"}}
               alt={`Cover for ${title}`} />
         </div>
-        <div className="space-y-10 md:py-10">
+        <div className="space-y-4 md:py-10">
           <div>
             <h1 className="text-3xl font-bold mb-2">{title}</h1>
             <p className="text-lightGreyBlue mb-5">作者: {author}</p>
             <p className="text-lg font-semibold text-primary">NT$ {price}</p>
           </div>
-          <div className="divider" />
-          <div>
-            <p className="text-sm mb-5 text-lightGreyBlue">內容簡介</p>
-            <p className="text-lg">{desc}</p>
-          </div>
-          <div className="flex items-end">
-            <div className="form-control mr-5">
+          <div className="flex flex-wrap items-end gap-5 max-w-[80%]">
+            <div className="form-control flex-1">
               <div className="qty-input">
                 <label htmlFor="qty-input-number" className="font-semibold">數量</label>
-                <div className="flex flex-row gap-2">
-                  <button data-action="decrement">
+                <div className="flex flex-row relative">
+                  <button data-action="decrement" className="btn rounded-l-l rounded-r-none">
                     <span className="m-auto text-3xl font-thin">-</span>
                   </button>
-                  <input type="text" placeholder="0" value="0" name="qty-input-number" className="input input-bordered font-semibold max-w-[80px] text-center" />
-                  <button data-action="increment">
+                  <input type="number" placeholder="0" value="0" name="qty-input-number" className="input input-bordered font-semibold text-center rounded-none focus:outline-none w-full px-1" />
+                  <button data-action="increment" className="btn rounded-l-none rounded-r-lg">
                     <span className="m-auto text-3xl font-thin">+</span>
                   </button>
                 </div>
               </div>
             </div>
-            <div className="grow-1 w-full">
+            <div className="flex-1">
               <button className="btn btn-secondary btn-block">Add to Cart</button>
             </div>
+          </div>
+          <div className="divider" />
+          <div>
+            <p className="text-sm mb-5 text-lightGreyBlue">內容簡介</p>
+            <p className="text-lg">{desc}</p>
           </div>
         </div>
 
