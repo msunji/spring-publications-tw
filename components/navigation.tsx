bc0c90b8f272ee5ad030698a56be5960702045ee
Marge <marge.consunji@gmail.com>
2023-06-02 13:13:55 +0800

    Add shopping cart to navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,17 +3,16 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="bg-darkBlue text-white">
-      <nav className="navbar container flex justify-between py-2">
+      <nav className="navbar container py-2">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-darkBlue rounded-box w-52">
               <li><Link href="/about">關於我們</Link></li>
               <li><Link href="/">全部商品</Link></li>
               <li><Link href="/contact">聯絡我們</Link></li>
-              <li><a>購物車</a></li>
             </ul>
           </div>
           <Link href="/" className="btn btn-ghost normal-case text-3xl">清泉</Link>
@@ -23,8 +22,10 @@ export default function Navbar() {
             <li><Link href="/about">關於我們</Link></li>
             <li><Link href="/">全部商品</Link></li>
             <li><Link href="/contact">聯絡我們</Link></li>
-            <li><a>購物車</a></li>
-          </ul>
+            </ul>
+        </div>
+        <div className="navbar-end">
+          <a>購物車</a><div className="badge badge-accent ml-2">0</div>
         </div>
       </nav>
     </div>
