8f3e060f3b1a26f9c1fa09aca6cf9518436200bc
Marge <marge.consunji@gmail.com>
2023-06-06 13:41:30 +0800

    Make featured books component

---
+++ components/featured.tsx
@@ -0,0 +1,9 @@
+export default function Featured() {
+  return(
+    <section className="bg-accent">
+      <div className="container mx-auto">
+        <h2>Featured Books</h2>
+      </div>
+    </section>
+  )
+}
