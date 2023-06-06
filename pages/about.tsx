318939506200711ed61fb6f9f7d48d5b6e6e4966
Marge <marge.consunji@gmail.com>
2023-06-06 13:59:56 +0800

    Fix container

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,40 +2,45 @@ export default function Page() {
   return (
     <>
       <section>
-      <h1>全部商品</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="container mx-auto">
+          <h1>全部商品</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </section>
       <section>
-        <h1>常見問題</h1>
-        <div>
-          <div className="collapse collapse-arrow border border-base-300 bg-base-200">
-            <input type="checkbox" /> 
-            <div className="collapse-title text-xl font-medium">
-              Payment Method
+        <div className="container mx-auto">
+          <h1>常見問題</h1>
+          <div className="faq-accordion">
+            <div className="collapse collapse-arrow border border-base-300 bg-base-200">
+              <input type="checkbox" /> 
+              <div className="collapse-title text-xl font-medium">
+                Payment Method
+              </div>
+              <div className="collapse-content"> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
             </div>
-            <div className="collapse-content"> 
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="collapse collapse-arrow border border-base-300 bg-base-200">
+              <input type="checkbox" /> 
+              <div className="collapse-title text-xl font-medium">
+                Delivery
+              </div>
+              <div className="collapse-content"> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
             </div>
-          </div>
-          <div className="collapse collapse-arrow border border-base-300 bg-base-200">
-            <input type="checkbox" /> 
-            <div className="collapse-title text-xl font-medium">
-              Delivery
-            </div>
-            <div className="collapse-content"> 
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-          <div className="collapse collapse-arrow border border-base-300 bg-base-200">
-            <input type="checkbox" /> 
-            <div className="collapse-title text-xl font-medium">
-              Ordering Books
-            </div>
-            <div className="collapse-content"> 
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="collapse collapse-arrow border border-base-300 bg-base-200">
+              <input type="checkbox" /> 
+              <div className="collapse-title text-xl font-medium">
+                Ordering Books
+              </div>
+              <div className="collapse-content"> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
             </div>
           </div>
         </div>
+
       </section>
     </>
   )
