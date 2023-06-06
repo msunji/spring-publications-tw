75c8e684437cb56d908fe70faf32e413abdd980c
Marge <marge.consunji@gmail.com>
2023-06-06 13:29:32 +0800

    Remove ternary bit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,13 @@ import Thumbnail from '@/components/thumbnail';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ data } : { data: Array<Book>}) {
-  const isLoading = useLoader();
   return (
     <section>
       <div>
         <div className="divider mb-20"><h1 className="text-center text-3xl">全部商品</h1></div>
         <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-8">
-          {
-            isLoading ? <p>Loading</p> :  (data.map((book:Book) => <Thumbnail key={book.id} {...book} />
-            ))
-          }
+          {data.map((book:Book) => <Thumbnail key={book.id} {...book} />
+            )}
         </div>
 
       </div>
