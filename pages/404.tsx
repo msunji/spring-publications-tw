b37430e4b6bf74ae80ec05886aaab3d533357b4f
Marge <marge.consunji@gmail.com>
2023-06-06 13:59:46 +0800

    Fix container

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Custom404() {
   return (
-    <section className="text-center">
+    <section className="text-center container mx-auto">
       <h1>404 錯誤</h1>
       <p>很抱歉，找不到網頁</p>
       <p>請<Link href="/">回到首頁</Link>。</p>
