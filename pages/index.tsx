f1c18d457b269e6c99da5691ba09e103020f8bd1
Marge <marge.consunji@gmail.com>
2023-06-06 13:28:55 +0800

    Remove loading and loading components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import { getBooks } from '@/lib/airtable'
 import { Book } from '@/types/types'
 import { Inter } from 'next/font/google'
-import Thumbnail from '@/components/thumbnail'
+import Thumbnail from '@/components/thumbnail';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ data } : { data: Array<Book>}) {
+  const isLoading = useLoader();
   return (
     <section>
       <div>
         <div className="divider mb-20"><h1 className="text-center text-3xl">全部商品</h1></div>
         <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-8">
-          { data.map((book:Book) => <Thumbnail key={book.id} {...book} />
-          )}
+          {
+            isLoading ? <p>Loading</p> :  (data.map((book:Book) => <Thumbnail key={book.id} {...book} />
+            ))
+          }
         </div>
+
       </div>
     </section>
   )
