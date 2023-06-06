2a51dacd60ac81df5cc6efa196a55924c1c7e2fa
Marge <marge.consunji@gmail.com>
2023-06-06 16:26:26 +0800

    Filter data and get featured books

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,31 @@ import { Book } from '@/types/types'
 import { Inter } from 'next/font/google'
 import Thumbnail from '@/components/thumbnail';
 import Featured from '@/components/featured';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ data } : { data: Array<Book>}) {
+  const featuredBooks = data.filter(book => {
+    if (book.categories) {
+      return book.categories.includes("Featured")
+    }
+  });
   return (
     <>
-      <Featured />
+      <Featured featuredList={featuredBooks}/>
       <section>
         <div className="container mx-auto">
-          <div className="divider mb-20"><h1 className="text-center text-3xl">全部商品</h1></div>
-          <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-8">
+          <div className="divider mb-20"><h1 className="text-center text-3xl">我們的商品</h1></div>
+          <div className="mb-20 grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-8">
             {data.map((book:Book) => <Thumbnail key={book.id} {...book} />
               )}
           </div>
-
+          <div className="text-center">
+            <Link href="/products">
+              <button className="btn">See All Books</button>
+            </Link>
+          </div>
         </div>
       </section>
     </>
@@ -26,7 +36,7 @@ export default function Home({ data } : { data: Array<Book>}) {
 
 export async function getServerSideProps() {
   try {
-    const res = await getBooks();
+    const res = await getBooks("Product List", "{Publish} = 1");
     const data = JSON.parse(JSON.stringify(res));
     return {
       props: {
