5148a514e9b33ee58a0695d7e3da29b183c53710
Marge <marge.consunji@gmail.com>
2023-06-07 15:46:06 +0800

    Add cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,8 +1,12 @@
 export default function Page() {
   return (
     <section>
-      <h1>購物車</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <div className="container">
+        <h1>購物車</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <div className="divider"></div>
+      </div>
+
     </section>
   )
 }
