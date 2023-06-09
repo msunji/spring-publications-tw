912029e42fe83791f6d71eabcef97d6e02b7606a
Marge <marge.consunji@gmail.com>
2023-06-09 12:43:29 +0800

    Show two columns on mobile

--- components/productGrid.tsx
+++ components/productGrid.tsx
@@ -7,7 +7,7 @@ type ProductProps = {
 
 export default function ProductGrid({ productData } : ProductProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-8 mb-10">
+    <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-8 mb-10">
       {productData.map((book:Book) => <Thumbnail key={book.id} {...book} />)}
     </div>
   )
