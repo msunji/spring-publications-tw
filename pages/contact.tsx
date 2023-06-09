94bc039717db3c74785ed334d7fdaaba7f775cdd
Marge <marge.consunji@gmail.com>
2023-06-09 18:00:30 +0800

    Add contact details

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,8 @@ export default function Page() {
       <div className="container mx-auto">
         <h1>聯絡我們</h1>
         <div>
-          Details to follow
+          <p><b>Email</b>: spring1902@yahoo.com</p>
+          <p><b>電話</b>: (02) 29391902</p>
         </div>
       </div>
     </section>
