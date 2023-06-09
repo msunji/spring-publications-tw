cdd4b8589b031ed08ca206c3442befbcdaf80008
Marge <marge.consunji@gmail.com>
2023-06-09 13:21:52 +0800

    Set typing for Hero component data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { getBooks } from '@/lib/airtable'
-import { Book } from '@/types/types'
+import { getBooks, getHero } from '@/lib/airtable'
+import { Book, HeroType } from '@/types/types'
 import { Inter } from 'next/font/google'
 import Thumbnail from '@/components/thumbnail';
 import Hero from '@/components/hero';
@@ -7,15 +7,10 @@ import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ data } : { data: Array<Book>}) {
-  // const featuredBooks = data.filter(book => {
-  //   if (book.categories) {
-  //     return book.categories.includes("Featured")
-  //   }
-  // });
+export default function Home({ data, heroContent } : { data: Array<Book>, heroContent: HeroType }) {
   return (
     <>
-      <Hero />
+      <Hero content={heroContent}/>
       <section>
         <div className="container mx-auto">
           <div className="divider mb-20"><h1 className="text-center text-3xl">我們的商品</h1></div>
@@ -38,9 +33,11 @@ export async function getServerSideProps() {
   try {
     const res = await getBooks("Product List", "{Publish} = 1", 8);
     const data = JSON.parse(JSON.stringify(res));
+    const heroContent = await getHero();
     return {
       props: {
-        data
+        data,
+        heroContent
       }
     }
   } catch (err) {
