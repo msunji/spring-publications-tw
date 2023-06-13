0bf04d29afebf5f3ccc27dc285d5aa43b6860dd1
Marge <marge.consunji@gmail.com>
2023-06-13 16:07:09 +0800

    Use Cloudinary url
    - Remove type guarding and update to use Cloudinary url

--- components/thumbnail.tsx
+++ components/thumbnail.tsx
@@ -3,22 +3,13 @@ import Image from 'next/image';
 import { Book } from '@/types/types';
 
 export default function Thumbnail ({ ...props }: Book) {
-  const { id, title, author, price } = props;
-
-  let thumbnailSrc;
-
-  if (props.thumbnail && typeof Array.isArray(props.thumbnail)) {
-    thumbnailSrc = props.thumbnail[0].thumbnails.large.url;
-  } else {
-    thumbnailSrc = "/images/PlaceholderCover.jpg";
-  }
-
+  const { id, title, author, thumbnail, price } = props;
   return (
     <Link href={`/products/${id}`}>
       <div className="transition-transform hover:scale-90">
         <Image
           className="mb-5"
-          src={thumbnailSrc}
+          src={thumbnail}
           width={450}
           height={650}
           style={{objectFit: "cover"}}
