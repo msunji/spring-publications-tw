ec6350f0c8d823bae58248026f8c566100aeaec8
Marge <marge.consunji@gmail.com>
2023-06-13 16:05:41 +0800

    Remove Thumbnail type
    - No longer using the Airtable attachment urls

--- types/types.ts
+++ types/types.ts
@@ -1,29 +1,29 @@
-export type Thumbnail = {
-  id: string;
-  width: number;
-  height: number;
-  url: string;
-  filename: string;
-  size: number;
-  type: string;
-  thumbnails: {
-    small: {
-      url: string,
-      width: number,
-      height: number
-    },
-    large: {
-      url: string,
-      width: number,
-      height: number
-    },
-    full: {
-      url: string,
-      width: number,
-      height: number
-    }
-  }
-}
+// export type Thumbnail = {
+//   id: string;
+//   width: number;
+//   height: number;
+//   url: string;
+//   filename: string;
+//   size: number;
+//   type: string;
+//   thumbnails: {
+//     small: {
+//       url: string,
+//       width: number,
+//       height: number
+//     },
+//     large: {
+//       url: string,
+//       width: number,
+//       height: number
+//     },
+//     full: {
+//       url: string,
+//       width: number,
+//       height: number
+//     }
+//   }
+// }
 
 export type Book = {
   id: string;
