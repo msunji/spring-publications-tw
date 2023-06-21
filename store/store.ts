0daf0ffc1331a2982f919b66c70f3bfbec1a4112
Marge <marge.consunji@gmail.com>
2023-06-21 10:58:44 +0800

    Add addToCart and removeFromCart actions

--- store/store.ts
+++ store/store.ts
@@ -8,27 +8,37 @@ type CartState = {
 }
 
 type CartActions = {
-  addToCart: any;
+  addToCart: (cartItem:CartItemType) => any;
+  removeFromCart: (cartItemId:string) => any;
 }
 
 export const useCartStore = create<CartState & CartActions>()((set) => ({
   cart: [],
   totalCost: 0,
   totalItems: 0,
+  // totalItems: cart.reduce((prev, curr) => prev + curr.quantity, 0),
   addToCart: (cartItem:CartItemType) => {
     set((state) => {
       const cart = [...state.cart];
-      const itemExists = cart.findIndex(item => item.id === cartItem.id);
-      console.log(itemExists);
-      if (itemExists !== -1) {
-        cart[itemExists].quantity++;
+      const existingItemIdx = cart.findIndex(item => item.id === cartItem.id);
+      if (existingItemIdx !== -1) {
+        cart[existingItemIdx].quantity++;
       } else {
         cart.push(cartItem)
       }
       return { cart }
     })
   },
-  // removeFromCart
+  removeFromCart: (cartItemId:string) => {
+    set((state) => {
+      const cart = [...state.cart];
+      const itemToRemoveIdx = cart.findIndex(item => item.id === cartItemId);
+      if (itemToRemoveIdx !== -1) {
+        cart.splice(itemToRemoveIdx, 1);
+      }
+      return { cart }
+    })
+  }
   // clearCart
   // addItem
   // decreaseItem
