d8feefbed4e5b418ad0d02a240cb234de3c7bc5a
Marge <marge.consunji@gmail.com>
2023-06-22 15:14:28 +0800

    Add all products header text, add styling to page select

--- components/paginated.tsx
+++ components/paginated.tsx
@@ -14,13 +14,18 @@ export default function Paginated({ paginatedData, currPage, pageLimit, totalPro
 
   return (
     <div>
+      <div className="mb-8">
+        <h1>全部商品</h1>
+        <p>您正在瀏覽第 1-12 件 商品</p>
+      </div>
+
       <ProductGrid productData={paginatedData} />
-      <div className="text-center">
-          <div className="join page-select">
+      <div className="text-left border-t border-base-200">
+          <div className="join page-select mt-8">
             { Array.from({ length: totalPages }).map((_, i) => {
               return (
                 <Link href={`/view-products/${i+1}`} key={i+1}>
-                  <button key={i} className="join-item btn">{i+1}</button>
+                  <button key={i} className="btn bg-transparent border-1 border-base-200 rounded-none text-neutral text-lg join-item hover:text-white hover">{i+1}</button>
                 </Link>
               )
             })}
