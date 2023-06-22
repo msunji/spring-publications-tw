9a48a343973ca0271dcee0be6bb0d12ea37146b1
Marge <marge.consunji@gmail.com>
2023-06-22 15:16:53 +0800

    Add cart styling

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,44 +2,54 @@ import { useEffect, useState } from "react";
 import { CartItemType } from '@/types/types';
 import { useCartStore } from '@/store/store';
 import { CartItems } from '@/components/cartItems';
+import Link from 'next/link';
 
 const emptyCart = () => {}
 
 export default function Page() {
   const { cart, totalItems, totalCost, clearCart } = useCartStore();
 
-  const [cartState, setCartState] = useState<Array<CartItemType>>();
+  const [cartState, setCartState] = useState<CartItemType[]>();
 
   useEffect(() => {
     setCartState(cart);
   }, [cart])
 
   return (
-    <section>
-      <div className="container">
-        <h1>購物車</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        <div className="grid grid-cols-[2fr_1fr] gap-8">
-          <div>
-            {
-              cartState?.length ? (<CartItems />) : (<div>Cart is Empty</div>)
-            }
+    <>
+      <section className="section-divider">
+        <div className="container">
+          <div className="flex items-center justify-between mb-10">
+            <h1 className="mb-0">購物車</h1>
+            <Link href="/view-products">繼續購物</Link>
           </div>
-          <div className="border p-5">
-            <h2>Contact Details</h2>
-            <button onClick={() => clearCart()}>X Clear Order</button>
+          <div>
             <div>
-              <b>Total</b>: {totalCost}
-              <button>
-                Finalise Order
-              </button>
+              {
+                cartState?.length ? (<CartItems />) : (<div>Cart is Empty</div>)
+              }
             </div>
-
+          </div>
+          <div className=" border-base-200 py-5 justify-end flex">
+            <div>
+              <p className="text-right text-lg">
+                <span className="mr-5 text-xl"><b>Subtotal</b></span> NT${totalCost}.00<br />
+              </p>
+              <p className="text-sm text-secondary">✱ Does not include shipping fee</p>
+            </div>
           </div>
         </div>
+      </section>
+      <section>
+        <div className="container">
+          <h1>Contact Details</h1>
 
-      </div>
+          <button className="btn btn-primary">
+            Submit Order
+          </button>
+        </div>
+      </section>
+    </>
 
-    </section>
   )
 }
