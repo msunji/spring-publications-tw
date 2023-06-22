e5886ff773e089344d9d610523e8c9c63a6609b6
Marge <marge.consunji@gmail.com>
2023-06-22 12:56:55 +0800

    Change bg colour

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,18 +3,18 @@ import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <footer className="bg-base-200">
+    <footer className="bg-offWhite">
       <div className="container footer p-10 text-base-content">
         <div>
           <Image src="/images/SpringLogo.png" width="130" height="140" alt="Spring Publications logo" />
           <p>清泉 Spring Publications<br/>Literature for human and spiritual growth</p>
         </div>
-        <div>
+        <div className="text-lg">
           <span className="footer-title">公司</span>
           <Link className="link link-hover" href="/about">關於我們</Link>
           <Link className="link link-hover" href="/contact">聯絡我們</Link>
         </div>
-        <div>
+        <div className="text-lg">
           <span className="footer-title">商品</span>
           <Link className="link link-hover" href="/about">全部商品</Link>
         </div>
