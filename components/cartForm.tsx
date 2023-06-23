ff5703ab4119495f367843289eb7da031d0a3f70
Marge <marge.consunji@gmail.com>
2023-06-23 15:29:29 +0800

    Move form to own component
    - Generate order ID
    - Pass form data
    - Still need to pass cart contents though

--- components/cartForm.tsx
+++ components/cartForm.tsx
@@ -1,15 +1,90 @@
-export default function CartForm() {
+import { useState } from "react";
+import { useCartStore } from '@/store/store';
+
+const generateOrderId = () => {
+  const typedArray = new Uint8Array(8);
+  const randomVals = window.crypto.getRandomValues(typedArray);
+  return randomVals.join('');
+}
+
+
+const handleSubmit = async (e: React.FormEvent) => {
+  e.preventDefault();
+  const form = e.target as HTMLFormElement;
+
+  const data = {
+    fullName: form.fullName.value as string,
+    email: form.email.value as string,
+    orderId: generateOrderId();
+  }
+  let error;
+  const { fullName, email } = data;
+  if (!fullName || fullName.trim() === "") {
+    alert('Please enter name');
+    error = "Please enter a valid name";
+    return error;
+  }
+  if (!email || email.trim() === "") {
+    alert("Please enter valid email address");
+    error = "Please enter a valid email address";
+    return error;
+  }
+
+  console.log(data);
+
+  const res = await fetch("/api/form", {
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST"
+  })
+
+  const result = await res.json();
+  console.log("form works", result.data);
+}
+
+
+export default function CartForm({ cartItems }: {cartItems: {}}) {
+  const [fullName, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
-    <form className="max-w-full md:max-w-[50%]">
-    <label className="block mb-2">
-      <span className="block text-sm font-medium text-slate-700 mb-1">Full Name</span>
-      <input type="text" value="Full Name" className="input input-bordered w-full rounded"/>
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-full md:max-w-[50%]"
+    >
+    <label className="block mb-2 flex align-" htmlFor="full-name">
+      <span className="block text-sm font-medium text-slate-700">Full Name</span>
     </label>
-    <label className="block mb-2">
-      <span className="block text-sm font-medium text-slate-700 mb-1">Email</span>
-      <input type="text" value="Email"  className="input input-bordered w-full rounded mb-2"/>
+    <input
+        id="fullName"
+        type="text"
+        placeholder="Full Name"
+        className="input input-bordered w-full rounded mb-2"
+        value={fullName}
+        minLength={2}
+        onChange={(e) => {
+          setFullname(e.target.value);
+        }}
+        required
+      />
+    <label className="block mb-2" htmlFor="email">
+      <span className="block text-sm font-medium text-slate-700">Email</span>
     </label>
-    <button className="btn btn-primary">
+    <input
+        id="email"
+        type="email"
+        placeholder="Email"
+        className="input input-bordered w-full rounded mb-4"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        minLength={2}
+        required
+      />
+    <button className="btn btn-primary" type="submit">
       Submit Order
     </button>
   </form>
