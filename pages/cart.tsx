6b188139541a14dedba67166c9bb16e9890d87c5
Marge <marge.consunji@gmail.com>
2023-06-23 11:42:53 +0800

    Additional styling, import form component

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { CartItemType } from '@/types/types';
 import { useCartStore } from '@/store/store';
 import { CartItems } from '@/components/cartItems';
+import CartForm from '@/components/cartForm';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -57,19 +58,7 @@ export default function Page() {
       <section>
         <div className="container">
           <h1>Contact Details</h1>
-          <form className="max-w-full md:max-w-[50%]">
-            <label className="block mb-2">
-              <span className="block text-sm font-medium text-slate-700 mb-1">Full Name</span>
-              <input type="text" value="Full Name" className="input input-bordered w-full rounded"/>
-            </label>
-            <label className="block mb-2">
-              <span className="block text-sm font-medium text-slate-700 mb-1">Email</span>
-              <input type="text" value="Email"  className="input input-bordered w-full rounded mb-2"/>
-            </label>
-            <button className="btn btn-primary">
-              Submit Order
-            </button>
-          </form>
+          <CartForm />
         </div>
       </section>
     </>
