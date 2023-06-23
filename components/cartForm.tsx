2385af2d1a9f33fc0c3a480d7495b6a3e7f3de14
Marge <marge.consunji@gmail.com>
2023-06-23 17:35:54 +0800

    Pass order info thru form

--- components/cartForm.tsx
+++ components/cartForm.tsx
@@ -1,19 +1,60 @@
 import { useState } from "react";
 import { useCartStore } from '@/store/store';
+import { CartItemType } from '@/types/types';
 
 const generateOrderId = () => {
-  let randomVal = window.crypto.getRandomValues(new Uint32Array(10)).toString();
+  let randomVal = String(window.crypto.getRandomValues(new Uint8Array(1)));
   return randomVal.substring(1,7);
 }
 
-const handleSubmit = async (e: React.FormEvent) => {
+// const handleSubmit = async (e: React.FormEvent) => {
+//   e.preventDefault();
+//   const form = e.target as HTMLFormElement;
+//   const cartDetails = form.dataset.cartData;
+
+//   const data = {
+//     fullName: form.fullName.value as string,
+//     email: form.email.value as string,
+//     orderId: generateOrderId(),
+//     cart: cartDetails
+//   }
+//   let error;
+//   const { fullName, email } = data;
+//   if (!fullName || fullName.trim() === "") {
+//     alert('Please enter name');
+//     error = "Please enter a valid name";
+//     return error;
+//   }
+//   if (!email || email.trim() === "") {
+//     alert("Please enter valid email address");
+//     error = "Please enter a valid email address";
+//     return error;
+//   }
+
+//   console.log(data);
+
+//   const res = await fetch("/api/form", {
+//     body: JSON.stringify(data),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     method: "POST"
+//   })
+
+//   const result = await res.json();
+//   console.log("form works", result.data);
+// }
+
+const handleSubmit = (orderData:{cart: Array<CartItemType>, totalCost: number}) => async (e:React.FormEvent) => {
   e.preventDefault();
   const form = e.target as HTMLFormElement;
 
   const data = {
     fullName: form.fullName.value as string,
     email: form.email.value as string,
-    orderId: generateOrderId()
+    orderId: generateOrderId(),
+    cartDetails: orderData.cart,
+    total: orderData.totalCost
   }
   let error;
   const { fullName, email } = data;
@@ -42,14 +83,19 @@ const handleSubmit = async (e: React.FormEvent) => {
   console.log("form works", result.data);
 }
 
-
 export default function CartForm({ cartItems }: {cartItems: {}}) {
   const [fullName, setFullname] = useState("");
   const [email, setEmail] = useState("");
+  const { cart, totalCost} = useCartStore();
+
+  const orderDetails = {
+    cart,
+    totalCost
+  }
 
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(orderDetails)}
       className="max-w-full md:max-w-[50%]"
     >
     <label className="block mb-2 flex align-" htmlFor="full-name">
