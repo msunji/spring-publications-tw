ad35cdd84ac18679611a76b95ec80e0200f27989
Marge <marge.consunji@gmail.com>
2023-06-26 16:09:51 +0800

    Send email through Sendgrid API
    - Sends email to both customer and store owner
    - Uses Sendgrid template for email styling

--- pages/api/submit-order.ts
+++ pages/api/submit-order.ts
@@ -1,20 +1,69 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+const sgMail = require("@sendgrid/mail");
+import { CartItemType } from '@/types/types';
 
 type ResponseData = {
   data: string
 }
 
+type OrderData = {
+  data: {
+    fullName: string,
+    email: string,
+    orderId: string,
+    cartDetails: Array<CartItemType>,
+    total: number
+  }
+}
+
+function sendMail(orderData : OrderData) {
+  sgMail.setApiKey(process.env.SENDGRID_API);
+  const { data } = orderData;
+  const msg = {
+    from: "mae.sunji@gmail.com",
+    text: "Thank you for ordering from Spring Books Taiwan!",
+    template_id: "d-8eb9a55a89e54d4eb8da1957044f3c86",
+    html: "<p>Thank you for ordering from Spring Books Taiwan</p>",
+    dynamic_template_data: {
+      data: data
+    },
+    personalizations: [
+      {
+        subject: "Spring Books Order Confirmation",
+        to: [
+          {
+            email: data.email
+          }
+        ],
+        bcc: [
+          {
+            email: "mae.sunji@gmail.com"
+          }
+        ]
+      }
+    ]
+  }
+  sgMail
+    .send(msg)
+    .then((res:NextApiResponse) => {
+      console.log("Message sent")
+    })
+    .catch((err:any) => {
+      console.log(err)
+    })
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { fullName, email, orderId, cartDetails, total } = req.body;
-  console.log("body: ", fullName, email, orderId, cartDetails, total);
+
   try {
     if(!fullName || !email || !cartDetails) {
       return res.status(400).json({
         data: "Customer details and cart details seem to be empty."
       })
     }
-    // res.status(200).json({ data: { fullName, email, orderId, cartDetails, total } });
-    res.redirect(307, "/thankyou");
+    sendMail({ data: { fullName, email, orderId, cartDetails, total } });
+    res.status(200).json({ data: { fullName, email, orderId, cartDetails, total } });
   } catch (err) {
     res.status(500).send({ error: "Failed to fetch data" });
   }
