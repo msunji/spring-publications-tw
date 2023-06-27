8b573959e34c6d2efeec71aaa187b907478b849b
Marge <marge.consunji@gmail.com>
2023-06-27 10:39:52 +0800

    Add order confirmation image

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,9 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <section>
-      <div className="container text-center">
+      <div className="container flex text-center flex-col items-center">
+        <Image
+          src="images/order-confirmed.svg"
+          height={200}
+          width={200}
+          alt="Person celebrating successful purchase"
+          className="mb-8"
+        />
         <h2>Thank you for ordering!</h2>
         <p className="mb-4">We will get back to you with the payment and delivery details when we can.</p>
         <Link href="/view-products">繼續購物</Link>
