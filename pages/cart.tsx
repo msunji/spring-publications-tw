ed39181647995bbaa5f5ab3e26d34ab35f0633e3
Marge <marge.consunji@gmail.com>
2023-06-27 14:24:25 +0800

    Change Contact Details to Delivery Details

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -57,7 +57,7 @@ export default function Page() {
       </section>
       <section>
         <div className="container">
-          <h1>Contact Details</h1>
+          <h1>運送資料</h1>
           <CartForm />
         </div>
       </section>
