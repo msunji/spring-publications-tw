879142758baf18c1bfc27234d70b8c9ba9a0071d
Marge <marge.consunji@gmail.com>
2023-06-29 20:31:02 +0800

    Rebuild form with React Hook Form

--- components/cartForm.tsx
+++ components/cartForm.tsx
@@ -1,180 +1,121 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useCartStore } from '@/store/store';
-import { CartItemType } from '@/types/types';
 import { nanoid } from 'nanoid';
 import { useRouter } from 'next/router';
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
-type InputProps = {
-  id: string;
-  name: string;
-  type: "text" | "number" | "email" | "tel";
-  errorMsg: string;
-  placeholderText?: string;
-  label: string;
-  minLen?: number;
-  regPattern?: string;
-  value: number | string;
-  onChange: (e:React.ChangeEvent<HTMLInputElement>) => void
+type FormData = {
+  fullName: string;
+  email: string;
+  mobile: string;
+  pickup: string;
 }
+
+const schema = yup
+  .object({
+    fullName: yup.string().required("Please input your full name"),
+    email: yup.string().email().required("Please input a valid email address"),
+    mobile: yup
+      .string()
+      .matches(/^09\d{8}$/, { message: "Please input a valid Taiwan mobile number."})
+      .min(9)
+      .required("Please input a valid Taiwan mobile number (e.g. 0912345678)"),
+    pickup: yup.string().required("Please input a valid address for pickup")
+  })
+  .required()
 
 const generateOrderId = () => {
   return nanoid(10)
 }
 
-const initialFormData = {
-  fullName: "",
-  email: "",
-  mobile: "",
-  pickup: "",
-}
-
-const CartFormInput = ({ id, name, type, errorMsg, label, minLen, placeholderText, regPattern, onChange, value } : InputProps) => {
-  const [error, setError] = useState("");
-
-  const handleInvalidity = (e:React.FormEvent) => {
-    const target = e.target as HTMLInputElement;
-    if (target.validationMessage) {
-      setError(errorMsg);
-    }
-  }
-
-  const handleBlur = (e:React.FormEvent) => {
-    const target = e.target as HTMLInputElement;
-    if (target.validationMessage) {
-      setError(errorMsg);
-    }
-  }
-  return (
-    <>
-      <label className="block my-2" htmlFor={name}>
-        <span className="block text-sm font-medium text-slate-700">{label}</span>
-      </label>
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholderText}
-        className="input input-bordered w-full rounded form-input"
-        onBlur={handleBlur}
-        onInvalid={handleInvalidity}
-        pattern={regPattern}
-        minLength={minLen}
-        onChange={onChange}
-        value={value}
-        required
-      />
-      <span className="text-sm text-error input-error-msg">{error}</span>
-    </>
-  )
-}
-
 export default function CartForm() {
-  const [formData, setFormData] = useState(initialFormData);
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors
+    }
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  });
 
   const { cart, total, clearCart } = useCartStore();
 
   const router = useRouter();
 
-  const orderDetails = {
-    cart,
-    total
-  }
-
-  // Event handlers
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
+  const onSubmit = (formData:FormData) => {
+    const data = {
       ...formData,
-      [e.target.id]: e.target.value
-    })
-  }
-
-  const handleSubmit = (orderData:{cart: Array<CartItemType>, total: number}) => async (e:React.FormEvent) => {
-    e.preventDefault();
-
-    const form = e.target as HTMLFormElement;
-    const isValid = form.checkValidity();
-
-    form.classList.add("submitted");
-
-    if (isValid) {
-      const data = {
-        fullName: form.fullName.value as string,
-        email: form.email.value as string,
-        mobile: form.mobile.value as string,
-        pickup: form.pickup.value as string,
-        orderId: generateOrderId() as string,
-        cartDetails: orderData.cart as Array<CartItemType>,
-        total: orderData.total as number
-      }
-      fetch("/api/submit-order", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        }
-      }).then(res => {
-        if (res.status === 200) {
-          router.replace("/thankyou");
-          clearCart();
-        }
-      }).catch(err => { console.error(err) })
+      orderId: generateOrderId(),
+      cartDetails: cart,
+      total
     }
+    fetch("/api/submit-order", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      }
+    }).then(res => {
+      if (res.status === 200) {
+        router.replace("/thankyou");
+        // clearCart();
+      }
+    }).catch(err => { console.error(err) })
+    return false;
   }
 
   return (
     <form
-      onSubmit={handleSubmit(orderDetails)}
+      onSubmit={handleSubmit(onSubmit)}
       className="max-w-full md:max-w-[50%]"
-      noValidate
     >
     <div>
-      <CartFormInput
+      <label htmlFor="fullName" className="block my-2">
+        <span className="block text-sm font-medium text-slate-700">姓名</span>
+      </label>
+      <input
         id="fullName"
-        label="姓名"
-        name="fullName"
-        type="text"
-        placeholderText="姓名 (required)"
-        minLen={1}
-        onChange={handleChange}
-        value={formData.fullName}
-        errorMsg="Please input your full name."
-      />
-      <CartFormInput
+        className="input input-bordered w-full rounded form-input"
+        placeholder="姓名 (required)"
+        aria-invalid={errors.fullName ? "true" : "false"}
+        {...register("fullName", { required: true })} />
+      <span className="text-sm text-error input-error-msg">{errors.fullName?.message}</span>
+
+      <label htmlFor="email" className="block my-2">
+        <span className="block text-sm font-medium text-slate-700">電子郵件</span>
+      </label>
+      <input
         id="email"
-        label="電子郵件"
-        name="email"
-        type="email"
-        placeholderText="電子郵件 (required)"
-        regPattern="[a-zA-Z0-9._-]*@[a-zA-Z]*\.[a-zA-Z]"
-        minLen={4}
-        onChange={handleChange}
-        value={formData.email}
-        errorMsg="Please input a valid email address."
-      />
-      <CartFormInput
+        className="input input-bordered w-full rounded form-input"
+        placeholder="電子郵件 (required)"
+        aria-invalid={errors.email ? "true" : "false"}
+        {...register("email", { required: true })} />
+      <span className="text-sm text-error input-error-msg">{errors.email?.message}</span>
+
+      <label htmlFor="mobile" className="block my-2">
+        <span className="block text-sm font-medium text-slate-700">手機號碼 (e.g. 0912345678)</span>
+      </label>
+      <input
         id="mobile"
-        label="手機號碼 (e.g. 0912345678)"
-        name="mobile"
-        type="tel"
-        placeholderText="手機號碼 (required)"
-        regPattern="[0-9\-\ ]{1,16}"
-        // regPattern="[0-9]"
-        minLen={9}
-        onChange={handleChange}
-        value={formData.mobile}
-        errorMsg="Please input a valid Taiwan mobile number (e.g. 0912345678)"
-      />
-      <CartFormInput
+        className="input input-bordered w-full rounded form-input"
+        placeholder="手機號碼 (required)"
+        aria-invalid={errors.mobile ? "true" : "false"}
+        {...register("mobile", { required: true })} />
+      <span className="text-sm text-error input-error-msg">{errors.mobile?.message}</span>
+
+      <label htmlFor="pickup" className="block my-2">
+        <span className="block text-sm font-medium text-slate-700">距離您家最近的便利商店 （7-11，、全家、OK Mart、萊爾富)</span>
+      </label>
+      <input
         id="pickup"
-        label="距離您家最近的便利商店 （7-11，、全家、OK Mart、萊爾富)"
-        name="pickup"
-        type="text"
-        placeholderText="距離您家最近的便利商店 (required)"
-        minLen={1}
-        onChange={handleChange}
-        value={formData.pickup}
-        errorMsg="Please input a valid address."
-      />
+        className="input input-bordered w-full rounded form-input"
+        placeholder="地址 (required)"
+        aria-invalid={errors.pickup ? "true" : "false"}
+        {...register("pickup", { required: true })} />
+      <span className="text-sm text-error input-error-msg">{errors.pickup?.message}</span>
     </div>
     <button className="btn btn-primary mt-4" type="submit" disabled={cart.length ? false : true}>
       Submit Order
